894e61bf0487f523004606063817ac47330e97b8
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-17 16:55:40 +0300

    add and read the text.txt file

---
+++ analyzer.rb
@@ -0,0 +1,9 @@
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
+
+puts "#{line_count} lines"
+
+total_characters = text.length
+
+puts "#{total_characters} characters"

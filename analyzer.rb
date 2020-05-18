322a4abc038bc9da1f34a769fa3765c37ae90d49
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-18 06:43:04 +0300

    Update analyzer.rb

--- analyzer.rb
+++ analyzer.rb
@@ -7,3 +7,9 @@ puts "#{line_count} lines"
 total_characters = text.length
 
 puts "#{total_characters} characters"
+
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospaces} characters (excluding spaces)"
+
+word_count = text.split.length
+puts "#{word_count} words"
